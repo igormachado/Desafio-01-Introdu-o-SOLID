161e7b3d2e78111898a6f4469bfa860f185d295b
Igor Machado <30518523+igormachado@users.noreply.github.com>
2021-12-23 18:18:16 -0300

    Add files via upload

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -20,22 +20,50 @@ class UsersRepository implements IUsersRepository {
 
   create({ name, email }: ICreateUserDTO): User {
     // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
     // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
     // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+
+    return user;
   }
 
+  // eslint-disable-next-line consistent-return
   turnAdmin(receivedUser: User): User {
     // Complete aqui
+    if (!receivedUser.admin) {
+      const user = Object.assign(receivedUser, {
+        admin: true,
+        updated_at: new Date(),
+      });
+
+      return user;
+    }
   }
 
   list(): User[] {
     // Complete aqui
+    return this.users;
   }
 }
 
