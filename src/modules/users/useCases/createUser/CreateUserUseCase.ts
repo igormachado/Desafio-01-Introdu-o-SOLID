7d5838dd6c913e01223ebe9b61d4ecee700449f2
Igor Machado <30518523+igormachado@users.noreply.github.com>
2021-12-23 18:20:11 -0300

    Add files via upload

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -11,6 +11,18 @@ class CreateUserUseCase {
 
   execute({ email, name }: IRequest): User {
     // Complete aqui
+    const user = this.usersRepository.findByEmail(email);
+
+    if (user) {
+      throw new Error("user already exists");
+    }
+
+    return this.usersRepository.create({
+      name,
+      email,
+    });
+
+    // return user;
   }
 }
 
